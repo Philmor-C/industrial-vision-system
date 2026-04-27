ecd2805272fa49ddb0246b453e852a183f419793
Philmor-C <filiydfz@gmail.com>
2026-04-27 19:35:54 +0800

    Update pipeline.py

--- industrial-vision-system/core/pipeline.py
+++ industrial-vision-system/core/pipeline.py
@@ -4,14 +4,42 @@ from core.fusion import fuse
 from core.anomaly_postprocess import heatmap_to_boxes
 from core.cv_measure import measure
 
-def run_pipeline(image, yolo_model, backbone, memory_bank):
+import torchvision.transforms as T
 
+transform = T.Compose([
+    T.Resize((224,224)),
+    T.ToTensor()
+])
+
+
+def run_pipeline(image, yolo_model, memory_bank, threshold):
+
+    # YOLO
     yolo_boxes = run_yolo(image, yolo_model)
 
-    heatmap, score = run_patchcore(image, backbone, memory_bank)
+    # PatchCore
+    img_tensor = transform(image).unsqueeze(0)
 
-    anomaly_boxes = heatmap_to_boxes(heatmap)
+    heatmap, score = run_patchcore(img_tensor, memory_bank)
 
+    anomaly_boxes = heatmap_to_boxes(heatmap, threshold)
+
+    # SCALE FIX (224 → original)
+    w, h = image.size
+    scale_x = w / 224
+    scale_y = h / 224
+
+    anomaly_boxes = [
+        (
+            int(x1 * scale_x),
+            int(y1 * scale_y),
+            int(x2 * scale_x),
+            int(y2 * scale_y),
+        )
+        for (x1,y1,x2,y2) in anomaly_boxes
+    ]
+
+    # Fusion
     matched, unknown = fuse(yolo_boxes, anomaly_boxes)
 
     measurement = measure(image)
@@ -22,6 +50,5 @@ def run_pipeline(image, yolo_model, backbone, memory_bank):
         "matched": matched,
         "unknown": unknown,
         "score": score,
-        "heatmap": heatmap,
         "measurement": measurement
     }
