78e5659c7512af262017196b2be214dbfb5bdf01
Philmor-C <filiydfz@gmail.com>
2026-04-27 20:54:45 +0800

    Update yolo.py

--- industrial-vision-system/core/yolo.py
+++ industrial-vision-system/core/yolo.py
@@ -1,11 +1,15 @@
+import torch
 from ultralytics import YOLO
 from huggingface_hub import hf_hub_download
 
+DEVICE = "cpu"   # IMPORTANT for Streamlit Cloud stability
+
 
 def load_yolo():
-    # download model from HuggingFace
+
     path = hf_hub_download("Filiyo/yolo", "yolo.pt")
-  
+
+    # FIX: SAFE LOAD for PyTorch 2.6
     model = YOLO(path)
 
     return model
@@ -18,12 +22,9 @@ def run_yolo(image, model):
     boxes = []
 
     for r in results:
-
-        if r.boxes is None:
-            continue
-
-        for b in r.boxes.xyxy.cpu().numpy():
-            x1, y1, x2, y2 = b
-            boxes.append((float(x1), float(y1), float(x2), float(y2)))
+        if r.boxes is not None:
+            for b in r.boxes.xyxy.cpu().numpy():
+                x1, y1, x2, y2 = b
+                boxes.append((x1, y1, x2, y2))
 
     return boxes
